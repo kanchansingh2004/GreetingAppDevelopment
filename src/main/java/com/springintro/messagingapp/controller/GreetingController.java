998eab830b36537b58486cb2b5970a57655bb1d8
kanchansingh2004 <kannursingh2004@gmail.com>
2025-02-24 12:48:29 +0530

    [Kanchan] Add. UC-02 use Services Layer to get
    Simple Greeting
    
    message

--- src/main/java/com/springintro/messagingapp/controller/GreetingController.java
+++ src/main/java/com/springintro/messagingapp/controller/GreetingController.java
@@ -1,7 +1,11 @@
 package com.springintro.messagingapp.controller;
 
+import com.springintro.messagingapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.*;
+import com.springintro.messagingapp.service.UserService;
+
 
 @RestController
 //Provide Base Path
@@ -30,5 +34,21 @@ public class GreetingController {
     @DeleteMapping("/delete")
     public String callDelete(){
         return "{ \" Message \" : \"Hello Buddy, greeting using delete request method!\" }";
+    }
+
+    //UC-02 use Services Layer to get Simple Greeting message
+    //automatically injects the UserService bean into this Controller
+    @Autowired
+    private UserService userService;
+
+    // Constructor-based Dependency Injection
+    public GreetingController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/getGreeting")
+    //calls the getGreeting method from UserService to get the greeting message
+    public String getMessage(){
+        return UserService.getGreeting();
     }
 }
